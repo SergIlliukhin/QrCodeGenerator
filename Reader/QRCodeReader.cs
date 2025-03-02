c7d4328a9d7ebf2d0c7fa3af0c04c4fe8487e6d6
Sergii Illiukhin <s.illyuhin@gmail.com>
2025-03-02 21:25:17 +0200

    Update QRCodeReader.cs

--- Reader/QRCodeReader.cs
+++ Reader/QRCodeReader.cs
@@ -13,10 +13,17 @@ public class QRCodeReaderUtil
         var height = bitmap.Height;
         var pixels = bitmap.Pixels;
         
-        var luminanceSource = new RGBLuminanceSource(
-            pixels.Select(p => (byte)((p.Red + p.Green + p.Blue) / 3)).ToArray(),
-            width, height);
+        // Convert SKColor pixels to RGB byte array (3 bytes per pixel)
+        var rgbBytes = new byte[width * height * 3];
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            var color = pixels[i];
+            rgbBytes[i * 3] = color.Red;     // R
+            rgbBytes[i * 3 + 1] = color.Green; // G
+            rgbBytes[i * 3 + 2] = color.Blue;  // B
+        }
             
+        var luminanceSource = new RGBLuminanceSource(rgbBytes, width, height);
         var binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
         var reader = new MultiFormatReader();
         var result = reader.decode(binaryBitmap);
